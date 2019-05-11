23a4e90798af741db39d44683cd987c43fa1d9f2
Greg Connell <greg.connell@ff0000.com>
2019-05-10 17:53:34 -0700

    redoes previous commit properly via BSA

--- src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557534438821,
+  "updated": 1557535985686,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
