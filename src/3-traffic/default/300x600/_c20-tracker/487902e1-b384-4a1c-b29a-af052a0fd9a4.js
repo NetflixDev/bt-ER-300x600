4b8e67f87bb75a4069901adcfbf262bf47506a43
Derek Miranda <derek.miranda@ff0000.com>
2019-05-23 18:56:26 -0700

    fix: ensures canvas animation on top
    - redist

--- src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1558660557982,
+  "updated": 1558662969709,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
