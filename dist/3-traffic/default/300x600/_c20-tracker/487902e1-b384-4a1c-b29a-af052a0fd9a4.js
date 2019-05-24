9d0c0942c6b37740767c388b77b83f0e8d3eb45f
Derek Miranda <derek.miranda@ff0000.com>
2019-05-23 18:16:15 -0700

    fix: removes Tune In from backup.json
    - redist

--- dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1558653889627,
+  "updated": 1558660557982,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
