8bfa5acc9f2587b409e80579b80c0939813b80ad
Derek Miranda <derek.miranda@ff0000.com>
2019-06-14 10:44:07 -0700

    fix: only plays supercut on desktop

--- src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ src/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -9,6 +9,7 @@ module.exports = {
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
+    "label": "300x600 NF Global Template",
     "tier": "endframe-resolve",
     "size": "300x600",
     "creative": "./dist/3-traffic/default/300x600",
@@ -19,7 +20,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1559262807778,
+  "updated": 1557970120016,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
