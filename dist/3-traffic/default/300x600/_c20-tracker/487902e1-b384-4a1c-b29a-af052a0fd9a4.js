0603b7de11f87882b37d888e199e189272077ff7
Derek Miranda <derek.miranda@ff0000.com>
2019-07-26 10:31:41 -0700

    feature: keyart first container animation
    - republish

--- dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1564008733115,
+  "updated": 1564162124418,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
