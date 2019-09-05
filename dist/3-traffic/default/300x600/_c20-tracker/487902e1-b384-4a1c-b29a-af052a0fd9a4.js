0ae5fd4307e456406a60d55c001d5f3df2885000
Derek Miranda <derek.miranda@ff0000.com>
2019-09-05 10:59:01 -0700

    fix: retraffic with reinstalled node_modules
    - redist too

--- dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1567546095144,
+  "updated": 1567706143350,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
