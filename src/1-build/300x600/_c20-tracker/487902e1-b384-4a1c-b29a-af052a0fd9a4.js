4e4c7dcabf3f2676e9c26c2d14adc510aa89e1f1
Derek Miranda <derek.miranda@ff0000.com>
2019-10-09 14:27:44 -0700

    Reorders manifest field order

--- src/1-build/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ src/1-build/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1570655221773,
+  "updated": 1570656456031,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
